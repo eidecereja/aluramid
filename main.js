0293dd8294770a2eae38476ec91bbf7be325e336
eidecereja <126575068+eidecereja@users.noreply.github.com>
2023-10-25 11:07:12 -0300

    programando main

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-function tocaSomAplausos(){
-    document.querySelector("#som_tecla_aplausos").play();
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
 const listaDeTeclas = document.querySelectorAll(".tecla");
 let contador = 0;
-while(contador < listaDeTeclas.length ){
-    listaDeTeclas[contador].onclick = tocaSomAplausos;
+while(contador < listaDeTeclas.length){
+    const efeito = listaDeTeclas[contador].classList[1];
+    const idAudio = "#som_"+efeito;
+    listaDeTeclas[contador].onclick = function (){
+        tocaSom(idAudio);
+    }
     contador = contador + 1;
-    console.log(contador);
