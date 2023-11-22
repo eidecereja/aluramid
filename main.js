4f404dc1ed75e751cf4cb9436e7138a2cb9154a8
eidecereja <126575068+eidecereja@users.noreply.github.com>
2023-11-22 11:02:41 -0300

    for

--- main.js
+++ main.js
@@ -12,11 +12,11 @@ for(let contador = 0; contador < listaDeTeclas.length; contador++){
     tecla.onclick = function(){
       tocaSom(idAudio);
     }   
-    tecla.onkeydown=function(evento){
+    tecla.onkeydown = function(evento){
       if(evento.code != 'Tab'){
       tecla.classList.add('ativa');
     }
-    tecla.onkeyup=function(){
+    tecla.onkeyup = function(){
       tecla.classList.remove('ativa');
     }
 
