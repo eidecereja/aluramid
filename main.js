e0f7c98f18a6f882b333203920c466c3b1aa7ce6
eidecereja <126575068+eidecereja@users.noreply.github.com>
2023-11-22 10:51:34 -0300

    alterando main

--- main.js
+++ main.js
@@ -11,5 +11,9 @@ for(let contador = 0; contador < listaDeTeclas.length; contador++){
     const idAudio = `#som_${efeito}`;
     tecla.onclick = function(){
       tocaSom(idAudio);
-    }    
+    }   
+    tecla.onkeydown=function(){
+      tecla.classList.add('ativa');
+    }
+       
 }
